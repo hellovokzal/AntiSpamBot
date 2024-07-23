e1303de22bacb87ed699baecd0d7c516e06b3925
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2024-07-23 23:35:53 +0500

    Update antispam.py

--- antispam.py
+++ antispam.py
@@ -4,9 +4,11 @@ from threading import Thread
 
 bot = telebot.TeleBot("7334734746:AAE_xWuTtPZOwt1VZCFUsH7L4FYu-zAc3BY")
 
-def g(usr, id):
+def g(usr, id, id_user):
     time.sleep(600)
     bot.send_message(id, f"@{usr}, следите за правилами чата и не нарушайте или могут в большой срок дать мут на 2 раза больше! 📢")
+    bot.restrict_chat_member(id, id_user, permissions=telebot.types.ChatPermissions(can_send_messages=True))
+    
 
 @bot.message_handler(commands=['m'])
 def h(m):
@@ -16,9 +18,9 @@ def h(m):
     # Проверка, является ли пользователь администратором
     chat_member = bot.get_chat_member(m.chat.id, m.reply_to_message.from_user.id)
     if chat_member.status != 'administrator':
-        bot.restrict_chat_member(m.chat.id, m.reply_to_message.from_user.id, until_date=int(time.time() + 600), permissions=telebot.types.ChatPermissions(can_send_messages=False))
+        bot.restrict_chat_member(m.chat.id, m.reply_to_message.from_user.id, until_date=int(time.time() + 10), permissions=telebot.types.ChatPermissions(can_send_messages=False))
         bot.send_message(m.chat.id, f"@{m.reply_to_message.from_user.username} заблокирован на 10 минут за нарушение правил. Будьте осторожны с правилами и следите за правилами! ⛔📢")
-        thr = Thread(target=g, args=(m.reply_to_message.from_user.username, m.chat.id))
+        thr = Thread(target=g, args=(m.reply_to_message.from_user.username, m.chat.id, m.reply_to_message.from_user.id))
         thr.start()
     else:
         bot.send_message(m.chat.id, "Невозможно ограничить права администратора.")
